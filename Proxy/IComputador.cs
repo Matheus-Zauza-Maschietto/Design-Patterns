da30d20ab56b6a7cb1d085a7b25f4a9c547c46a0
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-11-14 21:27:26 -0300

    Create IComputador.cs

---
+++ Proxy/IComputador.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.Proxy
+{
+    public interface IComputador
+    {
+        void Ligar();
+        void Desligar();
+        void AbrirNavegador();
+        void FecharNavegador();
+        
+    }
+}
