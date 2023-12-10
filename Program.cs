3d8e03d9d63d8a705ad3f64bebc16be9f75f8635
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-12-10 10:51:42 -0300

    design patterns

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-﻿using Design_Patterns.Flyweight;
+﻿using Design_Patterns.Decorator;
 
-FlyweightFactory factory = new();
+var decorator = new CoffeeDecorator(new Coffee());
 
-GeometricCalculator g1 = new("PI*R^2", factory.GetFlyweight("PI"));
-GeometricCalculator g2 = new("wdqd", factory.GetFlyweight("AUREA"));
-GeometricCalculator g3 = new("PI*R^2", factory.GetFlyweight("PI"));
-GeometricCalculator g4 = new("qwdqwd", factory.GetFlyweight("AUREA"));
-GeometricCalculator g5 = new("223*R^2", factory.GetFlyweight("AUREA"));
-factory.ShowCache();
+var milk = new CoffeeMilk(decorator);
+
+var sugar = new CoffeeSugar(milk);
+
+System.Console.WriteLine(sugar.());
